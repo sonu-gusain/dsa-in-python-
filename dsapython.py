5db7d48707ba0beb304f6586e48843a4bb76cd88
sonu-gusain <naveengusain0265@gmail.com>
2025-01-28 22:23:22 +0530

    easyquestion

--- dsapython.py
+++ dsapython.py
@@ -1,18 +1,9 @@
-def prime(n):
-    if n == 1:
-        return False
-    if n==2:
-        return True 
-    for i in range(3,n):
-        if n%2 == 0:
-            return False
-        if n%i == 0:
-            return False
-    return True
- 
-for j in range(1,101):
-    if prime(j):
-        print(j)
+#pattern 
+
+for i in range(1,5):
+    for j in range(1,5):
+        print(j,end ="")
+    print()
 
 
         
