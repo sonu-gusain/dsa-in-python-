c09021b2bf29ea750ef3f438c233b74a65aa22f7
sonu-gusain <naveengusain0265@gmail.com>
2025-01-28 23:13:46 +0530

    trangle pattern in dsa

--- dsapython.py
+++ dsapython.py
@@ -1,9 +1,10 @@
 # trangle pattern
 
-for i in range(1,5):
-    for j in range(1,5):
-        print(j,end ="")
+for i in range(5):
+    for j in range(i):
+        print("*",end="")
     print()
+
 
 
         
