b51f19b53f85be4b149d80db656389d806066b00
sonu-gusain <naveengusain0265@gmail.com>
2025-01-29 23:17:53 +0530

    inverted  trangle pattern

--- dsapython.py
+++ dsapython.py
@@ -1,12 +1,19 @@
-# trangle pattern
 
-for i in range(5):
-    for j in range(i,0,-1):
-        print(j,end="")
+#inverted  trangle pattern
+# 1111
+#  222
+#   33
+#    4
+
+for i in range(1,5):
+    for j in range(i-1):
+        print(" ",end = "")
+        
+    for k in range(5-i):
+        print(i,end="")
     print()
 
 
-        
 
 
         
