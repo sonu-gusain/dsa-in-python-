08224e6f90f3d0d70ad956e83890f14d5a7e77bc
sonu-gusain <naveengusain0265@gmail.com>
2025-01-30 12:14:27 +0530

    peramid trangle

--- dsapython.py
+++ dsapython.py
@@ -1,11 +1,17 @@
-count = 1
-for i in range(3):
-    for j in range(3):
-        print(count,end="")
-        count= count+1
+
+#peramid trangle 
+#    1
+#   121
+#  12321
+# 1234321
+
+for i in range(5):
+    for j in range(5-i-1):
+        print(" ",end ="")
+
+    for k in range(1,i+1):
+        print(k,end="")
+    for j in range(i-1,0,-1):
+        print(j,end="")
     print()
 
-        
-
-
-        
